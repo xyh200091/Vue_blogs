ed62a36aa7c0f157c36a5f71ac1c6bd318ebfb23
xu <2521929494qq.com>
2022-06-25 15:05:12 +0800

    '等待后端登录编写'

---
+++ src/views/Admin.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="admin">
+        <!-- 头部 -->
+        <div class="head">
+            <span>博客后台管理</span>
+            <button>退出登录</button>
+        </div>
+        <!-- 主体 -->
+        <div>
+            <!-- 目录 -->
+            <div></div>
+            <!-- 内容 -->
+            <div></div>
+        </div>
+        <!-- 登录 -->
+        <div class="login">
+            <div class="login_beg">
+                <span class="login_title">登录</span>
+                <input type="password" placeholder="输入密码" class="login_pwd">
+                <div>
+                    <input type="text">
+                    <img src="" alt="">
+                </div>
+                <button>登录</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'admin',
+    components: {
+
+    },
+    data: {
+
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    },
+}
+</script>
+
+<style scoped>
+@import './../../static/css/Admin.css'
+</style>
